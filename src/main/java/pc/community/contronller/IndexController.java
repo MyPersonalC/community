3bfc73ae122867aa230411cb5c444a72f28b2094
MyPersonalC <1095887877@qq.com>
2019-08-23 10:59:04 +0800

    correction indexController

--- src/main/java/pc/community/contronller/IndexController.java
+++ src/main/java/pc/community/contronller/IndexController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String index(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
+    @GetMapping("/")
+    public String index(){
         return "index";
     }
 
